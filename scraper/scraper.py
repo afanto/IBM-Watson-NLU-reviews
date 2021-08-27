bba2c1d1266304032f04d8f3a229d38e96dedbd5
afanto <85966358+afanto@users.noreply.github.com>
2021-08-27 14:16:13 +0200

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,37 +1,26 @@
-#imports: BS4, Selenium, Json
 import pymongo
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import json
 
-#Connect to MongoDB
-client = pymongo.MongoClient("mongodb+srv://username:password@clusterxxx.mongodb.net/database-name?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://_user:_password@_server/_database?retryWrites=true&w=majority")
 db = client.products
 
-#Product ID & number of reviews
 product_id = input("Enter product id: ")
 product_reviews = input("Enter number of reviews: ")
-
-number_of_pages = 1
-if (int(product_reviews) > 999):
-    number_of_pages = -(-int(product_reviews) // 1000)
+number_of_pages = -(-int(product_reviews) // 1000) if (int(product_reviews) > 999) else 1
+review_text, rating_value = [], []
 
 for page in range(number_of_pages):
 
-    #Launching webdriver via Selenium (Chrome)
     driver = webdriver.Chrome()
-    driver.get('https://www.iherb.com/ugc/api/review/adaptive?pid='+product_id+'&limit='+product_reviews+'&lc=en-US&page='+str(page)+'&sortId=2&withUgcSummary=true&withImagesOnly=false&isShowTranslated=false')
+    driver.get('https://www.iherb.com/ugc/api/review/adaptive?pid='+product_id+'&limit='+product_reviews+'&lc=en-US&page='+str(page+1)+'&sortId=2&withUgcSummary=true&withImagesOnly=false&isShowTranslated=false')
 
-    #Parsing page source and converting it to json format
-    soup = BeautifulSoup(driver.page_source,'html.parser')
-    site_json = json.loads(soup.text)
-
-    #Storing review text & ratings in two lists
-    review_text = [d.get('reviewText') for d in site_json['items'] if d.get('reviewText')]
-    rating_value = [d.get('ratingValue') for d in site_json['items'] if d.get('ratingValue')]
-
-    #Store reviews in MongoDB, two arrays
-    record = {'review_text': review_text, 'rating_value': rating_value}
-    db.ginko.insert_one(record)
+    site_json = json.loads(BeautifulSoup(driver.page_source,'html.parser').text)
+    review_text.append([d.get('reviewText') for d in site_json['items'] if d.get('reviewText')])
+    rating_value.append([d.get('ratingValue') for d in site_json['items'] if d.get('ratingValue')])
 
     driver.quit()
+
+reviews = {'review_text': review_text, 'rating_value': rating_value}
+db.ginko.insert_one(reviews)
