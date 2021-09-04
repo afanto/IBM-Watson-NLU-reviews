ce6f6e4deb3c6eb22e06140044530f320e63fcae
afanto <85966358+afanto@users.noreply.github.com>
2021-09-04 20:29:34 +0200

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,16 +1,18 @@
 from reviews_import import review_text_list, rating_value_list
 
-print("Number of reviews: " + str(len(rating_value_list)))
-
-review_text_list_5 = []
-
+review_text_positive = []
 for index in range(len(rating_value_list)):
-    if(rating_value_list[index] == 50):
-        review_text_list_5.append(review_text_list[index])
+   if(rating_value_list[index] == 50 or rating_value_list[index] == 40):
+        review_text_positive.append(review_text_list[index])
 
-print("Numer of 5 start reviews to be processed: " + str(len(review_text_list_5)))
+review_text_negative = []
+for index in range(len(rating_value_list)):
+    if(rating_value_list[index] == 10 or rating_value_list[index] == 20):
+        review_text_negative.append(review_text_list[index])
 
-review_text_list_5 = [s.rstrip() for s in review_text_list_5] #remove newline (/n)
-review_text_list_5 = [item for item in review_text_list_5 if len(item) > 50] #omit reviews shorter than 50 characters
+review_text_positive = [s.rstrip() for s in review_text_positive] #remove newline (/n)
+review_text_positive = [item for item in review_text_positive if len(item) > 50] #omit reviews shorter than 50 characters
+review_text_negative = [s.rstrip() for s in review_text_negative] #remove newline (/n)
+review_text_negative = [item for item in review_text_negative if len(item) > 50] #omit reviews shorter than 50 characters
 
-print("Numer of 5 start reviews to be processed (after cleaning): " + str(len(review_text_list_5)))
+no_reviews = len(review_text_list)
