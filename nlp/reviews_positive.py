4fd81a39f46cc972bf754145f16dd91581c52c24
afanto <85966358+afanto@users.noreply.github.com>
2021-09-11 15:09:25 +0200

    Update reviews_positive.py

--- nlp/reviews_positive.py
+++ nlp/reviews_positive.py
@@ -1,5 +1,7 @@
 from ibm_watson import ApiException
 from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOptions
+import pandas as pd
+import json
 
 from reviews import review_text_positive
 
@@ -11,22 +13,33 @@ def _positive(natural_language_understanding, relevance):
     score, label = [], []
     processed = no_positive_reviews
 
+    cols = ['label', 'score']
+    positive_df = pd.DataFrame(columns=cols)
+
     for it in range(len(review_text_positive)):
         try:
             response = natural_language_understanding.analyze(
                 text=review_text_positive[it],
                 features=Features(categories=CategoriesOptions())).get_result()
+            print(json.dumps(response, indent=2))
 
         except ApiException as ex:
             print("Method failed with status code " + str(ex.code) + ": " + ex.message)
             processed -= 1
 
+        l = list()
         for r in response['categories']:
+            r_copy = r
             if (int(r['score'] > relevance)):
                 score.append((r['score']))
                 label.append((r['label']))
+                r_copy["review"] = review_text_positive[it]
+                l.append(r_copy)
+
+        positive_df = positive_df.append(pd.DataFrame(data=l))
 
     print("Successfully processed " + str(processed) + " reviews.")
+    positive_df.to_csv("Ginkgo_positive.csv", sep='\t')
 
     label_merged, occurrences, score_avg = [], [], []
     [label_merged.append(x) for x in label if x not in label_merged]
